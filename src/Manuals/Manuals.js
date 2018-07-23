77d53e0045260d30b3f33f5fbe36007670b9c896
Unknown <magnus.ja.gustafsson@gmail.com>
2018-07-23 21:15:17 +0200

    Added manuels page

--- src/Manuals/Manuals.js
+++ src/Manuals/Manuals.js
@@ -1,12 +1,47 @@
-import React from 'react';
-
+import React from "react";
 
 const manuals = () => {
-    return (
-        <div>
-            manuals here
-        </div>
-    );
-}
+  const listStyle = {
+    paddingLeft: "20px",
+    marginTop: "0px",
+    marginBottom: "0px",
+    ":li": {
+      color: "red"
+    }
+  };
+
+  return (
+    <div>
+      <h1>StochSim Manuals:</h1>
+      <ul style={listStyle}>
+        <li>
+          <a href="StochSim_User's_Manual.pdf" target="_blank">StochSim manual</a> 
+          {" "}manual for the simulation language StochSim
+        </li>
+        <li>
+          <a href="StatRes.pdf" target="_blank">
+            StatRes manual
+          </a>{" "}
+          manual for the statistical tool StatRes
+        </li>
+        <li>
+          <a href="Optim.pdf" target="_blank">
+            Optim manual
+          </a>
+        </li>
+        <li>
+          <a href="Sensi.pdf" target="_blank">
+            Sensi manual
+          </a>
+        </li>
+        <li>
+          <a href="ParmVar.pdf" target="_blank">
+            ParmVar manual
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default manuals;
