9f326e57b07e6c1536690645efc8103cbf2702f1
Unknown <magnus.ja.gustafsson@gmail.com>
2019-08-20 12:29:01 +0200

    Updated homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,12 +4,15 @@ const home = (props) => {
   return (
     <div className={props.className}>
       <h1>
-        Purpose of <b>Stochastic CSS</b>
+        Purpose of Stochastic Simulation - StochSD 
       </h1>
       <p>
-        <b>Continuous System Simulation (CSS)</b> has long been an inferior and
-        inconsistent type of simulation compared to various forms of Discrete
-        Event Simulation (DES). See the paper:{" "}
+        Two main branches of simulation are <b>Continuous System Simulation (CSS)</b> and <b>Discrete Event Simulation (DES)</b>.<br />
+        <b>CSS</b> is a <i>macro approach</i> where many are lumped together in a structure of <b>compartments</b> connected by <b>flows</b>, where the compartments represent different stages of our interest. During the simulation, items are dynamically transferred  between the compartments. Traditionally, a CSS model was based on dif. equations, but with the <b>System Dynamics (SD)</b> philosophy, the model building could be directly performed by placing and connecting compartments and flows into a graphical structure.<br />
+        <b>DES</b>, on the other hand, is <i>micro approach</i> where each item is individually described in dynamic and stochastic terms.
+      </p>
+      <p>
+        CSS has long been an inferior and inconsistent type of simulation compared to various forms of DES. See the paper:{" "}
         <a 
           href="http://www.scirp.org/journal/PaperInformation.aspx?paperID=66005" 
           target="_blank"
@@ -18,60 +21,52 @@ const home = (props) => {
           <i>A guide to population modelling for simulation</i>
         </a>.
       </p>
+      <p>
       However, a CSS model can be fully consistent with a DES model by: <br />
       &nbsp; 1) modelling discrete objects as discrete,<br />
       &nbsp; 2) handling attributes and sojourn times correctly,<br />
       &nbsp; 3) including various types of stochasticities.<br />
-      <br />
-      <p>
-        Further, in all types of <b>stochastic simulation</b> you need to
-        perform <i>multiple simulations</i> of the model followed by a{" "}
-        <i>statistical analysis</i>. Therefore, a tool to perform this is required in
-        stochastic CSS modelling.
+      See the paper:{" "}
+      <a 
+          href="http://www.scirp.org/JOURNAL/PaperInformation.aspx?PaperID=80104" 
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i>The full potential of Continuous System Simulation modelling</i>
+        </a>.
       </p>
-      <br/>
+      <p>
+        The <b>purpose</b> of <b>StochSD</b> (<i>Stoch</i>astic <i>S</i>ystem <i>D</i>ynamics) is to provide an open source CSS/SD language that incorporates the stochastic means to build stochastic and dynamic models as well as necessary tools for multiple simulations followed by statistical analysis and result presentation. 
+      </p>
       <h2>
-        <b>StochSD</b> Software with documentation and Examples
+        StochSD Software with Documentation and Examples
       </h2>
-      In order to teach <i>stochastic</i> CSS modelling and simulation the open-source software {" "}
-      <b>StochSD</b> is provided <a href="/stochsd_software">here</a>.
-      <br />
-      <br />
-      <b>StochSD:</b> A CSS language for stochastic simulation
-      {" "}(including StatRes, which is a tool for statistical analysis of multiple
-      simulations).<br />
-      <br />
-      <b>Example Models</b> (presented in the paper, below): Six example models in
-      StochRes illustrating the need for stochactic CSS.
-      <br />
-      <br />
-      <b>Stochastic CSS:</b> Reference to the paper:{" "}
-      <a 
-        href="http://www.scirp.org/JOURNAL/PaperInformation.aspx?PaperID=80104"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i>The full potential of Continuous System Simulation modelling</i>
-      </a>
-      <br/>
-      <br/>
-      <a 
-        href="http://www.signal.uu.se/Research/simulation.html"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Research on Simulation, Uppsala University, Sweden
-      </a>
-      <br />
-      <br />
-      <b>Leif Gustafsson</b>
-      <br />
-      <a href="Mailto:Leif.Gunnar.Gustafsson@gmail.com">
-        Leif.Gunnar.Gustafsson@gmail.com
-      </a>
-      <br />
-      <br />
-      <i>Updated: 2018-07-25</i>
+      <p>
+        In order to use <i>stochastic</i> CSS/SD modelling and simulation the open-source software <b>StochSD</b> is provided <a href="/  stochsd_software">here</a>.
+      </p>
+      <p>
+        <b>Example Models</b> (presented in the paper ‘The full potential …’, referred to above): <br />
+        Six models in StochSD illustrating the need for correctly implemented stochactics in CSS.
+      </p>
+      <p>
+        <a 
+          href="http://www.signal.uu.se/Research/simulation.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Research on Simulation, Uppsala University, Sweden
+        </a>
+      </p>
+      <p>
+        <b>Leif Gustafsson</b>
+        <br />
+        <a href="Mailto:Leif.Gunnar.Gustafsson@gmail.com">
+          Leif.Gunnar.Gustafsson@gmail.com
+        </a>
+      </p>
+      <p>
+        <i>Updated: 2019-09-01</i>
+      </p>
     </div>
   );
 };
