a7707043b3296622f5feada0ab92af73921a1764
Unknown <magnus.ja.gustafsson@gmail.com>
2019-08-21 12:10:50 +0200

    Manual links are absolute paths

--- src/Components/Manuals/Manuals.js
+++ src/Components/Manuals/Manuals.js
@@ -1,9 +1,9 @@
 import React from "react";
-import StochSDUserManual from "./pdfs/StochSD_User_Manual.pdf";
-import StatResManual from "./pdfs/StochSD_StatRes.pdf";
-import OptimManual from "./pdfs/StochSD_Optim.pdf";
-import SensiManual from "./pdfs/StochSD_Sensi.pdf";
-import ParmVarManual from "./pdfs/StochSD_ParmVar.pdf";
+// import StochSDUserManual from "./pdfs/StochSD_User_Manual.pdf";
+// import StatResManual from "./pdfs/StochSD_StatRes.pdf";
+// import OptimManual from "./pdfs/StochSD_Optim.pdf";
+// import SensiManual from "./pdfs/StochSD_Sensi.pdf";
+// import ParmVarManual from "./pdfs/StochSD_ParmVar.pdf";
 
 const manuals = () => {
   const listStyle = {
@@ -18,32 +18,32 @@ const manuals = () => {
   return (
     <div>
       <h1>StochSD Manuals:</h1>
-      <a href={StochSDUserManual}><b>StochSD User's Manual</b></a> 
+      <a target="_blank" href={"https://stochsd.sourceforge.io/manuals/StochSD_User_Manual.pdf"}><b>StochSD User's Manual</b></a> 
         {" "}- for the simulation language StochSD.
       <br/>
       <br/>
       <b>Tools</b>
       <ul style={listStyle}>
         <li>
-          <a href={StatResManual}>
+          <a target="_blank" href={"https://stochsd.sourceforge.io/manuals/StochSD_StatRes.pdf"}>
             StatRes manual
           </a>
           {" "}- for multiple runs of a stochastic model, statistical analysis and result presentation.
         </li>
         <li>
-          <a href={ParmVarManual}>
+          <a target="_blank" href={"https://stochsd.sourceforge.io/manuals/StochSD_ParmVar.pdf"}>
             ParmVar manual
           </a>
           {" "}- for parameters estimation with a stochastic model.
         </li>
         <li>
-          <a href={OptimManual}>
+          <a target="_blank" href={"https://stochsd.sourceforge.io/manuals/StochSD_Optim.pdf"}>
             Optim manual
           </a>
           {" "}- for optimisation and for model fitting of a deterministic model.
         </li>
         <li>
-          <a href={SensiManual}>
+          <a target="_blank" href={"https://stochsd.sourceforge.io/manuals/StochSD_Sensi.pdf"}>
             Sensi manual
           </a>
           {" "}- for sensitivity analysis.
